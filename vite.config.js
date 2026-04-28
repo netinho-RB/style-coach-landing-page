294b9e03dcc7381f243640965ec21cf8ae189c06
nestorcode <nestornunespb10@gmail.com>
2026-04-27 21:13:24 -0300

    corrigindo bugs

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // O "base" garante que o site procure os arquivos na pasta correta do GitHub Pages
+  base: '/style-coach-landing-page/', 
+})
