dd5897490febbc259140ef8d70555d0193be99e4
nestorcode <nestornunespb10@gmail.com>
2026-04-27 21:27:57 -0300

    fix: use relative base path for universal deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // A barra no início e no fim do nome do repositório é essencial
-  base: '/style-coach-landing-page/', 
+  // O './' diz ao Vite: "procure os arquivos a partir de onde o index.html estiver"
+  base: './', 
 })
